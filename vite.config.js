e8f93bc040698deb3b41295afa9567764a4c3044
ksphere.world <ksphere.world@proton.me>
2026-07-06 14:17:45 +0530

    Setup GitHub Actions and Custom Domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ksphere.world/', // This MUST match your repository name!
+  base: '/', // Changed this to '/' for custom domains
   plugins: [
     react(),
     tailwindcss(),
